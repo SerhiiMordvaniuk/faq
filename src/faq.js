10747a66e05d9c6a7fb9aa12bba1c2cdb048c873
Serhii Mordvaniuk <sm636332@gmail.com>
2024-11-20 18:43:47 +0100

    dark

--- src/faq.js
+++ src/faq.js
@@ -10,3 +10,23 @@ new Accordion('.accordion-container', {
     },
   
 });
+
+
+const btn = document.querySelector(".btn");
+
+const theme = document.querySelector("#theme");
+
+btn.addEventListener("click", () => {
+  
+  if (theme.getAttribute("href") === "/css/light-styles.css") {
+    theme.href = "/css/dark.css";
+    localStorage.setItem("theme", "/css/dark-styles.css")
+  }
+  else {
+    theme.href = "/css/light-styles.css"
+    localStorage.setItem("theme", "/css/light-styles.css")
+
+  }
+
+}
+)
